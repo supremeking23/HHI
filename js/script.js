b12c1bfb286325134be703ee82dec65f2e966a62
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2018-12-01 22:07:25 +0800

    commit from asus

--- js/script.js
+++ js/script.js
@@ -1,63 +1,89 @@
 $(function(){
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+		/*$('.content-mission-vision').css('display','none');
+	    $('.site-logo').css('display','none');*/
+	    
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-       // document.getElementById("btnbacktoTop").style.display = "block";
-        $('#btnbacktoTop').fadeIn();
-    } else {
-        //document.getElementById("btnbacktoTop").style.display = "none";
-       
-        $('#btnbacktoTop').fadeOut();
-    }
-}
+	//
+	$('.tlt').textillate();
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    $('.main-body').waypoint(function(direction) {
+        if (direction == "down") {
+            //$('nav').addClass('sticky');
+             $('#btnbacktoTop').fadeIn();
+              $('header nav').addClass('inbody');
+        } else {
+           // $('nav').removeClass('sticky');
+           $('#btnbacktoTop').fadeOut();
+           $('header nav').removeClass('inbody');
+        }
+    }, {
+      offset: '60px;'
+    });	
 
-// scroll body to 0px on click
-$('#btnbacktoTop').click(function () {
-	$('body,html').animate({
-		scrollTop: 0
-	}, 800);
-	return false;
-});
+	// When the user scrolls down 20px from the top of the document, show the button
+	/*window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+	       
+	        $('#btnbacktoTop').fadeIn();
+	    } else {
+	       \
+	       
+	        $('#btnbacktoTop').fadeOut();
+	    }
+	}*/
+
+	// When the user clicks on the button, scroll to the top of the document
+	/*function topFunction() {
+	    document.body.scrollTop = 0;
+	    document.documentElement.scrollTop = 0;
+	}*/
+
+	// scroll body to 0px on click
+	$('#btnbacktoTop').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
 
 
 
-$('[data-tooltip="tooltip"]').tooltip();
-//alert(1);
-
- 
+	$('[data-tooltip="tooltip"]').tooltip();
+	//alert(1);
 
 
 
-  $(window).scroll(function() {
+	/* Animations on scroll */
+	$('.content-mission-vision').waypoint(function(direction) {
+	    $('.content-mission-vision').addClass('animated fadeIn');
+	    $('.site-logo').addClass('animated fadeInUp');
+	    
+	}, {
+	    offset: '50%'
+	});
+
+
+	$('.features').waypoint(function(direction) {
+	    $('.features').addClass('animated fadeIn');
+	   
+	    
+	}, {
+	    offset: '50%'
+	});
+
+
+ /* $(window).scroll(function() {
 	    var y_scroll_pos = window.pageYOffset;
 	    var scroll_pos_test = 520;   
-	    //console.log(y_scroll_pos);   
-	   // var mobile_view = 235;
-
-	//	   alert(1);
-
-		// set to whatever you want it to be
-
 	    if(y_scroll_pos > scroll_pos_test ) {
-		  // $("body").css("background-color","#000");
-		    $('header nav').addClass('inbody');
-	    }
-		else
-		{
-			//$("body").css("background-color","#FFF");
-			//alert(2);
-			  $('header nav').removeClass('inbody');
+		   $('header nav').addClass('inbody');
+	    }else{
+			$('header nav').removeClass('inbody');
 		}
-	});	
+	});	*/
 
 
 	/*$('.navbar-toggle').on('click',function(){
