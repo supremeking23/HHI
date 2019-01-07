a0d9442a23d1baad15efee9233bee5ab71f7bafa
unknown <sstinfra@SST-MBL100.sandstone.local>
2019-01-07 13:00:24 +0800

    with admin dashboard

--- js/demo.js
+++ js/demo.js
@@ -16,6 +16,6 @@ $.contactButtons({
   effect  : 'slide-on-scroll',
   buttons : {
     'facebook':   { class: 'facebook', use: true, link: 'https://www.facebook.com/Hunters-Hub-Incorporated-208732030013230/', extras: 'target="_blank"' },
-    'linkedin':   { class: 'linkedin', use: true, link: 'https://www.facebook.com/Hunters-Hub-Incorporated-208732030013230/', extras: 'target="_blank"'  },
+    'linkedin':   { class: 'linkedin', use: true, link: 'https://www.linkedin.com/company/hunter-s-hub-incorporated/about/', extras: 'target="_blank"'  },
   }
 });
